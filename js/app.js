bb6d61ad44a3ef240860edbd06b5fb429ac1982d
Adrian31 <adrian_carabetta@hotmail.com>
2017-10-19 12:44:19 +0800

    Added the data binding to show cat level in the index file and the app file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 var viewModel = function() {
+  var self = this;
+
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
+  self.catLevel = ko.observable('NewBorn');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   this.imgAttribution = ko.observable('https://flickr.com/photos/big');
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
+    var count = 0;
+    count++;
   };
+
+    self.catLevel = ko.computed(function(){
+      var clicks = self.clickCount();
+      var title = self.catLevel();
+
+      if (clicks < 10) {
+        return ("NewBorn");
+      } else if (clicks >= 10 && clicks < 15) {
+        return("Infant");
+      }
+  });
 };
 
 ko.applyBindings(new viewModel());
