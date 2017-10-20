7c995ff463bb8c677726300114bc1810a1ed99cf
Adrian31 <adrian_carabetta@hotmail.com>
2017-10-20 13:12:17 +0800

    added a Cat function and changed the viewModel so it's serperate from the Cat function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var viewModel = function() {
+var Cat = function() {
   var self = this;
 
   self.clickCount = ko.observable(0);
@@ -6,37 +6,53 @@ var viewModel = function() {
   self.catLevel = ko.observable('NewBorn');
   self.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   self.imgAttribution = ko.observable('https://flickr.com/photos/big');
+  self.nicknames = ko.observableArray([
+        { name: 'Fabby' },
+        { name: 'Tabby' },
+        { name: 'Shabby' }
+    ]);
 
-  self.incrementCounter = function() {
-    self.clickCount(self.clickCount() + 1);
-    var count = 0;
-    count++;
-  };
+
+
 
     self.catLevel = ko.computed(function(){
       var clicks = self.clickCount();
       var title = self.catLevel();
 
       if (clicks < 10) {
-        return ("NewBorn");
-      } else if (clicks >= 10 && clicks < 15) {
-        return("Infant");
+        //return ("NewBorn");
+        title = 'Newborn';
+      } else if (clicks < 20) {
+        title = 'Infant';
       }
+      else if (clicks < 50) {
+        title = 'Child';
+      }
+      else if (clicks < 100) {
+        title = 'Teen';
+      }
+      else if (clicks < 150) {
+        title = 'Adult';
+      }
+      else if (clicks < 200) {
+        title = 'Ninja';
+      }
+
+      return title;
   });
 
-  self.people = ko.observableArray([
-        { name: 'Fabby' },
-        { name: 'Tabby' },
-        { name: 'Shabby' }
-    ]);
+}
 
-    /*self.addPerson = function() {
-        self.people.push({ name: "New at " + new Date() });
-    };
+var viewModel = function() {
+  var self = this;
 
-    self.removePerson = function() {
-        self.people.remove(this);
-    }*/
+  self.currentCat = ko.observable(new Cat() );
+
+  self.incrementCounter = function() {
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
 };
+
 
 ko.applyBindings(new viewModel());
